a3f41967ade160b969fe09c8b7aca2ddeeab90c0
김민재 <kimminje7267@naver.com>
2024-07-28 16:40:07 +0900

    feat: todo 추가 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useReducer, useState } from 'react'
+
 import './App.css'
 import InputWithButton from './components/InputWithButton'
 import {
@@ -9,16 +11,55 @@ import {
   TableHeader,
   TableRow,
 } from './components/ui/table'
+import { getTime } from './utils/getTime'
+
+interface Todo {
+  genDate: string
+  genTime: string
+  status: string
+  content: string
+  deadTime: string
+}
+
+const initialState: Todo[] = []
+
+function reducer(state: Todo[], action: { type: string; todo: Todo }) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [...state, action.todo]
+    case 'RESET_TODO':
+      return []
+    default:
+      throw new Error()
+  }
+}
 
 function App() {
+  const [todos, dispatch] = useReducer(reducer, initialState)
+  const [content, setContent] = useState('')
+
+  const handleClick = () => {
+    const newTodo = {
+      genDate: getTime().slice(0, 11),
+      genTime: getTime().slice(12, 23),
+      status: '대기중',
+      content: content,
+      deadTime: 'N/A',
+    }
+    dispatch({ type: 'ADD_TODO', todo: newTodo })
+  }
+
+  const handleChange = (content: string) => {
+    setContent(content)
+  }
+
   return (
     <div className="flex flex-col items-center justify-center space-y-10">
       <div className="flex w-full max-w-sm items-center justify-center space-x-2">
         <InputWithButton
           placeholder="할 일을 입력하세요!"
-          handleClick={() => {
-            console.log('Button clicked')
-          }}
+          handleChange={handleChange}
+          handleClick={handleClick}
         />
       </div>
       <div>
@@ -26,19 +67,23 @@ function App() {
           <TableCaption></TableCaption>
           <TableHeader>
             <TableRow>
-              <TableHead className="w-[100px]">생성시간</TableHead>
+              <TableHead>생성날짜</TableHead>
+              <TableHead>생성시간</TableHead>
               <TableHead>상태</TableHead>
               <TableHead>내용</TableHead>
               <TableHead className="text-right">마감시간</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            <TableRow>
-              <TableCell className="font-medium">15:17</TableCell>
-              <TableCell>진행중</TableCell>
-              <TableCell>신용카드 신청하기</TableCell>
-              <TableCell className="text-right">19:00</TableCell>
-            </TableRow>
+            {todos.map((todo, index) => (
+              <TableRow key={index}>
+                <TableCell className="font-medium">{todo.genDate}</TableCell>
+                <TableCell className="font-medium">{todo.genTime}</TableCell>
+                <TableCell>{todo.status}</TableCell>
+                <TableCell>{todo.content}</TableCell>
+                <TableCell className="text-right">{todo.deadTime}</TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </div>
