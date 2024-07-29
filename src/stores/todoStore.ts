8b45f549aa21fe235176e3b3ca31de05e2f1478b
김민재 <kimminje7267@naver.com>
2024-07-29 22:22:00 +0900

    feat: useTodoStore에 updateTodo, deleteTodo 메서드 추가

--- src/stores/todoStore.ts
+++ src/stores/todoStore.ts
@@ -7,6 +7,8 @@ import type { Todo } from '@/types/todo'
 type TodoStore = {
   todos: Todo[]
   addTodo: (todo: Omit<Todo, 'id'>) => void
+  updateTodo: (id: string, updatedTodo: Todo) => void
+  deleteTodo: (id: string) => void
   resetTodos: () => void
 }
 
@@ -17,6 +19,16 @@ export const useTodoStore = create<TodoStore>()(
       addTodo: (todo) =>
         set((state) => ({
           todos: [...state.todos, { ...todo, id: Date.now().toString() }],
+        })),
+      updateTodo: (id, updatedTodo) =>
+        set((state) => ({
+          todos: state.todos.map((todo) =>
+            todo.id === id ? { ...todo, ...updatedTodo } : todo,
+          ),
+        })),
+      deleteTodo: (id) =>
+        set((state) => ({
+          todos: state.todos.filter((todo) => todo.id !== id),
         })),
       resetTodos: () => set({ todos: [] }),
     }),
