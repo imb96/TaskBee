d9efa715aeab2af801b3543756b2be45bd144ea3
김민재 <kimminje7267@naver.com>
2024-07-31 22:14:52 +0900

    style: 변수 중복 선언 제거

--- src/utils/getTime.ts
+++ src/utils/getTime.ts
@@ -1,5 +1,4 @@
 export const getTime = () => {
   const now = new Date()
-  const seoulTime = now.toLocaleString('ko-KR', { timeZone: 'Asia/Seoul' })
-  return seoulTime
+  return now.toLocaleString('ko-KR', { timeZone: 'Asia/Seoul' })
 }
